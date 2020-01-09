9fd911e32962ab1225df22830762b4afc1ebb08e
Robert Jherick Lim <robert.jherick.lim@gmail.com>
2020-01-10 02:54:35 +0800

    Added card component for displaying list of cats per breed

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-md-3 col-sm-6 col-12">
+    <div class="card mt-2">
+      <img :src="imgUrl" width="" class="card-img-top" />
+      <div class="card-body">
+        <router-link
+          :to="{ name: 'cat', params: { catId: id } }"
+          class="btn btn-primary btn-block"
+          >View Details</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id", "imgUrl"]
+};
+</script>
