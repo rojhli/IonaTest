fb35908b0dc10423b1ef60b8d9ce6b30a0bd5cfc
Robert Jherick Lim <robert.jherick.lim@gmail.com>
2020-01-10 02:53:01 +0800

    Added vuex module to handle state for cat API

---
+++ src/store/modules/cat.js
@@ -0,0 +1,69 @@
+const state = {
+  list: [],
+  count: 0,
+  pageCount: 1,
+  currentPageCount: 1
+};
+
+const actions = {
+  getById({ commit, state }, { id, isPagination = false }) {
+    let page = 0;
+    let runMutations = response => {
+      commit("setCount", parseInt(response.headers["pagination-count"]));
+      commit("setPageCount");
+
+      commit("setCatList", response.data);
+    };
+
+    if (isPagination) {
+      page = state.currentPageCount;
+
+      runMutations = response => {
+        commit("incrementCurrentPageCount", 1);
+        commit("appendCatList", response.data);
+      };
+    }
+
+    this._vm.$axios
+      .get("images/search", {
+        params: {
+          page,
+          limit: 10,
+          breed_ids: id,
+          order: "desc"
+        }
+      })
+      .then(response => {
+        runMutations(response);
+      });
+  }
+};
+
+const mutations = {
+  setCatList(state, cats) {
+    state.list = cats;
+  },
+
+  appendCatList(state, cats) {
+    state.list = state.list.concat(cats);
+  },
+
+  setCount(state, count) {
+    state.count = count;
+  },
+
+  setPageCount(state) {
+    state.pageCount = Math.ceil(state.count / 10);
+  },
+
+  incrementCurrentPageCount(state, n) {
+    state.currentPageCount += n;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+};
